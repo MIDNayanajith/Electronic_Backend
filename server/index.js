b579d33b9c9643687eebf32343977f6ed48cd004
MIDNayanajith <isuranga_2020736@fot.ruh.ac.lk>
2025-04-13 13:34:53 +0530

    develop add profile of user

--- server/index.js
+++ server/index.js
@@ -5,9 +5,10 @@ import itemRoutes from "./routes/item.js";
 import supplierRoutes from "./routes/suppliers.js";
 import purchaseRoutes from "./routes/purchase.js";
 import saleRoutes from "./routes/sale.js";
+import invoiceRoutes from "./routes/invoice.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import path from "path";
 const app = express();
 
 app.listen(8800, () => {
@@ -27,9 +28,12 @@ app.use(
 );
 app.use(cookieParser());
 
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/items", itemRoutes);
 app.use("/api/suppliers", supplierRoutes);
 app.use("/api/purchase", purchaseRoutes);
 app.use("/api/sale", saleRoutes);
+app.use("/api/invoice", invoiceRoutes);
---
+++ server/upload.js
@@ -0,0 +1,25 @@
+// upload.js
+import multer from "multer";
+import path from "path";
+
+// Set storage engine
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/user"); // Make sure this folder exists
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+// Filter to allow only images
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed!"), false);
+  }
+};
+
+export const upload = multer({ storage, fileFilter });
